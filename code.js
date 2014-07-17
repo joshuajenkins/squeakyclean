fe4915ea3e1fa7463c8aa1028cb441c8d8eac86a
Joshua Jenkins <josh@joshuajenkins.com>
2014-07-17 16:49:34 -0700

    Jeeze @daneden threw a fit

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 (function(){
   var el = document.createElement('div');
   el.id = 'squeky-clean';
-  el.style.position = 'absolute';
-  el.style.width = '100vw';
-  el.style.height = '100vh';
+  el.style.position = 'fixed';
   el.style.backgroundColor = 'transparent';
   el.style.top = 0;
+  el.style.right = 0;
+  el.style.bottom = 0;
   el.style.left = 0;
   el.style.zIndex = 99999;
   document.body.appendChild(el);
