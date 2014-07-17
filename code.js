9bfe7b9dbdfb3edb284a2c60ecfc6db59b3421fe
Joshua Jenkins <josh@joshuajenkins.com>
2014-07-17 16:22:04 -0700

    Clean up the web

---
+++ code.js
@@ -0,0 +1,11 @@
+(function(){
+  var el = document.createElement('div');
+  el.id = 'squeky-clean';
+  el.style.position = 'absolute';
+  el.style.width = '100vw';
+  el.style.height = '100vh';
+  el.style.backgroundColor = 'transparent';
+  el.style.top = 0;
+  el.style.left = 0;
+  document.body.appendChild(el);
+}());
